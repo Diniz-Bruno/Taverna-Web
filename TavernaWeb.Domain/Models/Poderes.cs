00c3dd69ceee5112780bb26670fff3765ce957e2
Diniz-Bruno <bruno.ribeiro.076@ufrn.edu.br>
2026-09-11 15:50:53 -0300

    feat(domain):ajuste do model de poderes
    -colocado Custo de Pe

--- TavernaWeb.Domain/Models/Poderes.cs
+++ TavernaWeb.Domain/Models/Poderes.cs
@@ -5,5 +5,6 @@ public class Poderes
     public int Id { get; set; }
     public int Idficha { get; set; }        
     public string Nome { get; set; } = string.Empty;
+    public int CustoPe { get; set; }
     public string? Descricao { get; set; }
 }
