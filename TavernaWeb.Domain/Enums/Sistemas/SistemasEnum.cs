37ab4d0b8e8404904473b89e769b3aff1c98cf81
Diniz-Bruno <brunoribeirodiniz076@gmail.com>
2026-09-12 15:48:33 -0300

    feat(domain): estruturar model Ficha e vinculo com Ordem
    - Adiciona SistemasEnum para tipagem dos sistemas suportados
    - Atualiza Ficha com IdUsuario, Sistema e CriadoEm
    - Adiciona chave Idficha e Sistema no model Ordem para composicao

---
+++ TavernaWeb.Domain/Enums/Sistemas/SistemasEnum.cs
@@ -0,0 +1,8 @@
+namespace TavernaWeb.Domain.Enums.Sistemas
+{
+    public enum SistemasEnum
+    {
+        ORDEM_PARANORMAL = 1
+        //COLOCA DE ACORDO COM NOVOS SISTEMAS IMPLEMENTADOS
+    }
+}
